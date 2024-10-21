aaf6513004d5546a3af3042547307ae97fd8ddbf
Ye Bhone Lin <106800189+Ye-Bhone-Lin@users.noreply.github.com>
2024-10-21 18:20:35 +0630

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('README.md','r') as f:
+  description = f.read()
+
 setup(
-    name='Siamese', # this is the package folder that contain the "main.py"
-    version='0.1', # to increment this version number for each new version of this package
+    name='SimboloSiamese', # this is the package folder that contain the "main.py"
+    version='0.1.1', # to increment this version number for each new version of this package
     packages=find_packages(), 
     install_requires=[ # dependencies for this package
     # e.g. 'numpy>=1.11.1'
@@ -10,4 +13,6 @@ setup(
     entry_point={"console_scripts": [
       "Siamese = Siamese:*",
     ],},
+    long_description=description,
+    long_description_content_type= "text/markdown",
 )
